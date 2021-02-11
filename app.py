1bf7cb039188cb0b16ad606894cad80baf04e583
Polarwinkel <it@polarwinkel.de>
2021-02-11 23:07:32 +0100

    fix: drop dead connections

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# TODO: Qt WebChannel checken!
 '''
 Flask wsgi-interface for mdTeXerPad
 Base file of mdTeXerPad:
@@ -13,6 +12,7 @@ import logging
 from contextvars import ContextVar
 from flask import Flask, render_template, request, send_from_directory, make_response, jsonify
 from flask_sockets import Sockets # Not working on Ubuntu 20.04 due to an old version of gevent
+import asyncio
 import json
 from modules import mdTeX2html
 
@@ -32,8 +32,9 @@ host='0.0.0.0'
 
 # WebServer stuff:
 
-@app.route('/<path:path>', methods=['GET'])
-def index(path):
+@app.route('/', methods=['GET'])
+@app.route('/edit', methods=['GET'])
+def index():
     app.logger.info('http connection: '+str(request.remote_addr))
     return render_template('page.html')
 
@@ -62,22 +63,26 @@ def mdtex_socket(ws):
     app.logger.info('ws connected: '+str(request.remote_addr))
     if ws not in USERS:
         USERS.add(ws)
-    if ws.closed:
-        USERS.remove(ws)
-    while (USERS != False):
+    while True:
         message = ws.receive()
         if message != None:
             mdtex.value = str(message)
         html = mdTeX2html.convert(mdtex.value)
         replyd = {'mdtex': mdtex.value, 'html': html, 'users': str(len(USERS))}
-        replyj = json.dumps(replyd)        
-        for user in USERS:
-            # TODO: one thread per user?
-            try:
-                user.send(replyj)
-            except:
-                USERS.remove(ws)
-                app.logger.warn('I did not reach a ws-client, (is kicked now)')
+        replyj = json.dumps(replyd)
+        asyncio.run(sendUpdate())
+
+async def sendUpdate():
+    html = mdTeX2html.convert(mdtex.value)
+    replyd = {'mdtex': mdtex.value, 'html': html, 'users': str(len(USERS))}
+    replyj = json.dumps(replyd)        
+    for ws in USERS: # TODO use .copy() or similar, but that might cause an error as well when trying to send no non-existing.
+        try:
+            ws.send(replyj)
+        except:
+            #send() raises a ConnectionClosed exception when the client disconnects which will exit the while True loop
+            USERS.remove(ws)
+            app.logger.warn('I did not reach a ws-client; I kicked it!')
 
 # run it:
 
